39ec58420e36e0ae93ac191be12c9bbfd8315506
alaa-100 <alaaredafarouq204_sd@fcis.bsu.edu.eg>
2024-02-22 05:53:38 -0800

    done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     import sys
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': Edg/121.0.0.0'}
     try:
         response = requests.get(url, headers=headers,
                                 allow_redirects=False)
